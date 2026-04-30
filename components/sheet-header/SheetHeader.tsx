4cd388d1a5404836d585027add7c8cad8ca74a56
lluaslua <luangfanjos@Gmail.com>
2026-04-30 09:44:49 -0300

    feat: create SheetHeader Component.

--- components/sheet-header/SheetHeader.tsx
+++ components/sheet-header/SheetHeader.tsx
@@ -1,14 +1,31 @@
 'use client'
 
+import { ElementType } from "react";
 import { Card } from "../ui/card"
+import { CharacterSheet } from "@/types/character-sheet.types"
+import { Button } from "../ui/button";
+import { Pen, PenLine  } from "lucide-react"
 
-export default function SheetHeader(){
+interface SheetHeaderProps{
+    data: CharacterSheet;
+    icon?: ElementType;
+}
+
+export default function SheetHeader({ data, icon: Icon }: SheetHeaderProps) {
 
 
     return(
-        <Card>
-            <div className="rounded-100 w-1 h-1 bg-white"></div>
-            <h1>Oi</h1>
+        <Card className="flex flex-row p-4 justify-between items-center">
+            <div className="flex flex-row items-center gap-3">
+            <div className="flex items-center justify-center w-[90px] h-[90px] rounded-full bg-black overflow-hidden flex-shrink-0 p-4">
+                    {Icon && <Icon className="text-white opacity-50 w-full w-full" />}
+                </div>
+            <div className="flex-col gap-1">
+            <h1>{data.name}</h1>
+            <p className="text-ds-label text-primary-foreground">{data.playerClass} - Nível {data.level} - {data.playerRace}</p>
+            </div>
+            </div>
+            <Button><PenLine/>Editar Ficha</Button>
         </Card>
     )
 }
